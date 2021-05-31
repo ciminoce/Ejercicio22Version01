52a9a1cbeb2d7e27ee63e46f5c7a93a620c4214e
ciminoce <ciminoce@yahoo.com.ar>
2021-05-31 20:49:48 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio22Version01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio22Version01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            double temperaturaActual, temperaturaConvertida;
+            Console.WriteLine("Opciones Disponibles");
+            Console.WriteLine("(C)elsius a Fahrenheit");
+            Console.WriteLine("(F)ahrenheit a Celsius");
+            Console.Write("Ingrese C o F:");
+            var opcionTemperatura = Console.ReadLine();
+            if (opcionTemperatura.ToUpper()=="C" || opcionTemperatura.ToUpper()=="F")
+            {
+                if (opcionTemperatura.ToUpper()=="C")
+                {
+                    Console.Write("Ingrese una temperatura en grados centígrados:");
+                    temperaturaActual = Convert.ToDouble(Console.ReadLine());
+                    temperaturaConvertida = 1.8 * temperaturaActual + 32;
+                    Console.WriteLine($"{temperaturaActual} ºC equivalen a {temperaturaConvertida} grados F");
+                }
+                else
+                {
+                    Console.Write("Ingrese una temperatura en gados Fahrenheit:");
+                    temperaturaActual = Convert.ToDouble(Console.ReadLine());
+                    temperaturaConvertida= (temperaturaActual - 32) * 5 / 9;
+                    Console.WriteLine($"{temperaturaActual} ºF equivalen a {temperaturaConvertida} grados C");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Opción no válida... Fin del programa");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
